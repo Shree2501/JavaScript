dab077f2d2bba94b850472609a90f30b67b6b6a6
Shree Sharma <sharmashree750@gmail.com>
2026-02-10 14:23:04 +0530

    Data Types

---
+++ dataTypes.js
@@ -0,0 +1,9 @@
+let name="Shree"
+let age=25
+const isStudent=true
+let address
+console.log(typeof name)      // string
+console.log(typeof age)       // number
+console.log(typeof isStudent) // boolean
+console.log(typeof address)  // undefined
+console.log(typeof null)     // object
