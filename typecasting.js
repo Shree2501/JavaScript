1217b1bff8c3130d8578fe62f1c6098f86b936fd
Shree Sharma <sharmashree750@gmail.com>
2026-02-11 19:15:15 +0530

    Typecasting

---
+++ typecasting.js
@@ -0,0 +1,12 @@
+let n = 33
+console.log(typeof n) // number
+console.log(n);
+
+
+let a = String(n)
+console.log(typeof a) // string
+console.log(a);
+
+let b = Boolean(n)
+console.log(typeof b) // boolean
+console.log(b); 
