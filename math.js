0f0e3a2e257ef2c756b9e6c4ce17ec3a363cd4cd
Shree Sharma <sharmashree750@gmail.com>
2026-02-12 19:02:57 +0530

    Math function

---
+++ math.js
@@ -0,0 +1,23 @@
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(4.9));
+// console.log(Math.trunc(4.7));
+// console.log(Math.sign(-4));
+
+// console.log(Math.pow(8, 2));
+// console.log(Math.sqrt(64));
+// console.log(Math.abs(-4.7));
+
+// console.log(Math.sin(90 * Math.PI / 180));
+// console.log(Math.cos(0 * Math.PI / 180));
+
+// console.log(Math.min(0, 150, 30, 20, -8, -200));
+// console.log(Math.min(0, 150, 30, 20, -8, -200));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()* 10) + 1)
+
+a=Math.log(1)
+console.log(a);
+
+
