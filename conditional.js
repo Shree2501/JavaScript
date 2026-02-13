99016c583b63fe52c956d31234ade1e3abc47856
Shree Sharma <sharmashree750@gmail.com>
2026-02-13 17:12:44 +0530

    Conditional Statements

---
+++ conditional.js
@@ -0,0 +1,51 @@
+// if statement
+// if (true) {
+//     console.log("executed");
+// }
+
+// if-else statement
+// if (2 === "2"){
+//     console.log("Equal")
+// }
+// else{
+//     console.log("not equal");
+// }
+
+// if-else if-else statement
+// let age = 20
+// if(age < 18){
+//     console.log("Not eligible to vote")
+// }
+// else if(age >= 18){
+//     console.log("Eligible to vote")
+// }
+// else{
+//     console.log("Please enter valid age")
+// }
+
+const weekday = 2
+switch(weekday){
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday")
+        break
+    case 3:
+        console.log("Wednesday")
+        break
+    case 4:
+        console.log("Thursday")
+        break
+    case 5:
+        console.log("Friday")
+        break
+    case 6:
+        console.log("Saturday")
+        break
+    case 7:
+        console.log("Sunday")
+        break
+    default:
+        console.log("Not a valid choice")
+}
