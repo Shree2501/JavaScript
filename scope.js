2eb36880c93855477b61401016836d75b4f42d01
Shree Sharma <sharmashree750@gmail.com>
2026-02-16 17:13:16 +0530

    Global and Local scope

---
+++ scope.js
@@ -0,0 +1,15 @@
+let a = 10
+if (true){
+    let a = 30
+    // console.log("inner",a);   
+}
+// console.log(a);
+
+let c = 5;
+function myfunc() {
+  const c = 3;
+}
+myfunc();
+console.log(c);
+
+
