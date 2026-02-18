271adb37df7ec12349a71cced0a2d8ed531227f0
Shree Sharma <sharmashree750@gmail.com>
2026-02-18 21:19:43 +0530

    Stack & Heap

---
+++ stack&heap.js
@@ -0,0 +1,13 @@
+let username = "shree25"
+let anotherUsername = username
+anotherUsername = "shree.sharma"
+console.log("Username:", username)
+console.log("Another Username:", anotherUsername)
+let userOne = {
+    email : "shree@gmail.com",
+    password : "s3cr3tP@ssw0rd"
+}
+let userTwo = userOne
+userTwo.password = "newP@ssw0rd"
+console.log("User1 Password:", userOne.password)
+console.log("User2 Password:",userTwo.password)
